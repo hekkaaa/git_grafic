792754d5d2725ce27db52e3756591990233af385
Alexander Pototsky <silencemyalise@gmail.com>
2021-07-30 17:12:49 +0600

    +request

--- main.py
+++ main.py
@@ -8,10 +8,5 @@ async def root():
     return {"message": "Hello World"}
 
 
-if __name__ == '__main__':
-    uvicorn.run(
-        "app:app",
-        host='localhost',
-        port=8080,
-        reload=True
-    )
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
