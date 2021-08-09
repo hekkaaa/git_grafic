411bdf84b6e4faaf3c4911ec2f7ed933383542f8
Alexander Pototsky <silencemyalise@gmail.com>
2021-08-09 22:43:52 +0600

    Добавление прелоадера по нажатию кнопки.

--- js/testscript.js
+++ js/testscript.js
@@ -1,7 +1,29 @@
+
+// Нажатие кнопки
 button1.onclick = function(){
-    let div = document.createElement('div');
-    div.className = "alert";
-    div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+    // let div = document.createElement('div');
+    // div.className = "spinner";
+    // div.innerHTML = '<div class="cube1"></div> <div class="cube2"></div>';
+    // document.body.append(div); //добавляем элементы
   
-    document.body.append(div);
+    
+    // setTimeout(() => testtext.remove(), 1000);// удаляем
+
+    startpageelem1.remove()
+    startpageelem2.remove()
+    button1.remove()
+
+    preloaders()
+    
+
+}
+
+
+function preloaders(){
+
+    let div = document.createElement('div');
+    div.className = "spinner";
+    div.innerHTML = '<div class="cube1"></div> <div class="cube2"></div>';
+    document.body.append(div); //добавляем элементы
+    setTimeout(() => div.remove(), 1000);
 }
