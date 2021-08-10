2c0a6e0b390414c3f6ba0ca802c24abb4d7f9352
Alexander Pototsky <silencemyalise@gmail.com>
2021-08-11 01:56:23 +0600

    test edit

--- js/testscript.js
+++ js/testscript.js
@@ -29,8 +29,15 @@ function preloaders(){
     // setTimeout(() => div.remove(), 1000);
 }
 
-function fetchresponce (){
+async function fetchresponce(){
+    let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+    let response = await fetch(url);
 
+        if (response.ok) {
+            return true
+        } else {
+            return false
+        }
 }
 
 const StartAnim = {
@@ -49,18 +56,18 @@ const StartAnim = {
            //запуск прелоадера
             setTimeout(() => preloaders(), 1600)
             setTimeout(() => this.show = true, 4000)
+
+            let hh = fetchResponse()
+            console.log(hh)
         }
     }
 } 
 
 Vue.createApp(StartAnim).mount('#startpagevue')
 
-// const Demo = {
-// data() {
-//     return {
-//     show: true
-//     }
-// }
-// }
 
-// Vue.createApp(Demo).mount('#demo')
+
+
+
+  
+ 
