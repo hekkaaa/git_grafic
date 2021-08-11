3487a6fa0e708f3f443143e664e331464b0f1b26
Alexander Pototsky <silencemyalise@gmail.com>
2021-08-12 00:31:50 +0600

    Добавил корректную анимацию с ошибкой от сервера.

--- js/testscript.js
+++ js/testscript.js
@@ -24,11 +24,13 @@ function preloaders(){
 
     let div = document.createElement('div');
     div.className = "spinner";
+    div.id = 'preloader';
     div.innerHTML = '<div class="cube1"></div> <div class="cube2"></div>';
     document.body.append(div); //добавляем элементы
     // setTimeout(() => div.remove(), 1000);
 }
 
+// Запрос на сервер
 async function fetchresponce(){
     let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
     let response = await fetch(url);
@@ -40,34 +42,45 @@ async function fetchresponce(){
         }
 }
 
+/// VUE JS
 const StartAnim = {
     data(){
         return {
-            show: true
+            show: true, // переод анимации
+            servresp: 'none', // переменная для ошибки от сервера при запросе
+            isActive: true // отображение ошибки
         }
     },
     methods: {
         async StartClick(){
             this.show = false
-            // удаление элементов 
-            // setTimeout(() => testtext.remove(), 2000)
-            // setTimeout(() => startpageelem2.remove(), 2000)
-            // setTimeout(() => button1.remove(), 2000)
-
+    
            //запуск прелоадера
             setTimeout(() => preloaders(), 1600)
-            setTimeout(() => this.show = true, 4000)
-
+            
             // делаем запрос к серверу через функцию.
             let response = await this.Testfett()
 
             // логика ответа от сервера.
             if(response.ok){
                 console.log('true')
+                // удаление элементов 
+                // setTimeout(() => testtext.remove(), 2000)
+                // setTimeout(() => startpageelem2.remove(), 2000)
+                // setTimeout(() => startpageelem3.remove(), 2000)
+                // setTimeout(() => button1.remove(), 2000)
             }
             else{
-                console.log('false')
+                // если ответа нет убираем preloader и возвращаем меню.
+                setTimeout(() => this.show = true, 2500)
+                // удаление
+                setTimeout(() => preloader.remove(), 2500)
+                this.isActive = false
+                this.servresp = response.status
+                  
             }
+        
+               
            
         },
         // запрос к серверу.
@@ -76,14 +89,12 @@ const StartAnim = {
             let response = await fetch(url);        
             return response
         }
-    }
+    },
+    mounted() {
+        setInterval(() => {
+            this.status = true 
+        }, 1000)
+      }
 } 
 
-Vue.createApp(StartAnim).mount('#startpagevue')
-
-
-
-
-
-  
- 
+Vue.createApp(StartAnim).mount('#startpagevue') 
